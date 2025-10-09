d36198ee76c6b6fd57656f1b825781e0967542d5
Jordain <153791495+jjcm296@users.noreply.github.com>
2025-10-09 23:54:57 +0000

    feat: agregar Suspense a ArtistsPage para resolver problema de carga en Vercel

--- front/src/app/artists/page.js
+++ front/src/app/artists/page.js
@@ -1,13 +1,16 @@
-'use client';
-
+import { Suspense } from "react";
 import ArtistsHeader from "@/features/artists/components/ArtistsHeader";
 import ArtistsFilters from "@/features/artists/components/ArtistsFilters";
 
 export default function ArtistsPage() {
-    return (
-        <main style={{ padding: "24px" }}>
-            <ArtistsHeader />
-            <ArtistsFilters/>
-        </main>
-    );
+  return (
+    <main style={{ padding: "24px" }}>
+      <Suspense fallback={<div>Cargando encabezado...</div>}>
+        <ArtistsHeader />
+      </Suspense>
+      <Suspense fallback={<div>Cargando filtros...</div>}>
+        <ArtistsFilters />
+      </Suspense>
+    </main>
+  );
 }
