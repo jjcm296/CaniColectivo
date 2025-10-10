2a33733e03fce8d265e63720da4fff88c5d572a2
jjcm296 <jordanjaircruzmendoza@gmail.com>
2025-10-10 17:13:50 -0600

    style: se hicieron mas pequeños los botones

--- front/src/app/artists/page.js
+++ front/src/app/artists/page.js
@@ -1,11 +1,12 @@
+import { Suspense } from "react";
 import ArtistsHeader from "@/features/artists/components/ArtistsHeader";
-import ArtistsFilters from "@/features/artists/components/ArtistsFilters";
 
 export default function ArtistsPage() {
     return (
         <main style={{ padding: "24px" }}>
-            <ArtistsHeader />
-            <ArtistsFilters/>
+            <Suspense fallback={<div>Cargando encabezado...</div>}>
+                <ArtistsHeader />
+            </Suspense>
         </main>
     );
 }
