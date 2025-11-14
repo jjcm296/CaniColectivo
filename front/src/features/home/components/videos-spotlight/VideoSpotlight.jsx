b7d8607a5f33ede9c346fbe3098180cc10455c34
jjcm296 <jordanjaircruzmendoza@gmail.com>
2025-11-13 18:20:07 -0600

    feat(VideoSpotlight): se agregaron más videos

--- front/src/features/home/components/videos-spotlight/VideoSpotlight.jsx
+++ front/src/features/home/components/videos-spotlight/VideoSpotlight.jsx
@@ -11,7 +11,16 @@ const DEFAULT_VIDEOS = [
         url: 'https://www.tiktok.com/@cani.colectivo/video/7555272248484007175?_r=1&_t=ZS-91LoQueQKQx',
         title: 'Cani',
     },
-
+    {
+        id: 'v2',
+        url: 'https://www.tiktok.com/@cani.colectivo/video/7559994915350973704',
+        title: 'Mi amiga',
+    },
+    {
+        id: 'v3',
+        url: 'https://www.tiktok.com/@cani.colectivo/photo/7568908647720291602?embed_source=121374463%2C121468991%2C121439635%2C121749182%2C121433650%2C121404359%2C121497414%2C121477481%2C121351166%2C121947600%2C121811500%2C121896267%2C121860360%2C121487028%2C121331973%2C120811592%2C120810756%2C121885509%3Bnull%3Bembed_masking&refer=embed&referer_url=localhost%3A3000%2F&referer_video_id=7559994915350973704',
+        title: 'Momentos con la comunidad',
+    },
 ];
 
 export default function VideoSpotlight({ videos = DEFAULT_VIDEOS }) {
