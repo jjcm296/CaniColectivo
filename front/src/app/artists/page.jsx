e06301b31316bd648cba7d24eab80afa86e692a8
jjcm296 <jordanjaircruzmendoza@gmail.com>
2025-12-08 19:11:55 -0600

    prueba 2

--- front/src/app/artists/page.jsx
+++ front/src/app/artists/page.jsx
@@ -1,13 +1,19 @@
-'use client'
+"use client";
 
+import { Suspense } from "react";
 import ContentHeader from "@/features/ui/content-header/ContentHeader";
 import ArtistsGrid from "@/features/artists/componensts/artists-grid/ArtistsGrid";
 
 export default function ArtistsPage() {
     return (
         <main style={{ padding: "24px" }}>
+            <Suspense fallback={<p>Cargando filtros...</p>}>
                 <ContentHeader scope="artists" />
+            </Suspense>
+
+            <Suspense fallback={<p>Cargando Artistas...</p>}>
                 <ArtistsGrid />
+            </Suspense>
         </main>
     );
 }
